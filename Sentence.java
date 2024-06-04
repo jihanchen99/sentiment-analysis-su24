03bee787abd142cdcb8c3ce50b6356ff27b63379
jihanchen <jihanc@seas.upenn.edu>
2024-06-03 22:28:59 -0400

    Overrides equals and hashCode in Sentence.java

--- Sentence.java
+++ Sentence.java
@@ -1,3 +1,4 @@
+import java.util.Objects;
 
 /** 
  * @author Chris Murphy
@@ -32,5 +33,20 @@ public class Sentence {
 		return text;
 	}
 
-	
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) {
+			return true;
+		}
+		if (!(o instanceof Sentence)) {
+			return false;
+		}
+		Sentence sentence = (Sentence) o;
+		return score == sentence.score && Objects.equals(text, sentence.text);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(score, text);
+	}
 }
